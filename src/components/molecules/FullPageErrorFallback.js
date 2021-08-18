bf7136a3d1e52d05ff8ee303b3c73ab0ef154d0f
niyaz <niaz203898@gmail.com>
2021-08-18 22:04:56 +0600

    Fix FullPageErrorFallback boundary styles not applying

--- src/components/molecules/FullPageErrorFallback.js
+++ src/components/molecules/FullPageErrorFallback.js
@@ -1,22 +1,35 @@
+/** @jsxImportSource @emotion/react */
+
 import { useTheme } from '@emotion/react'
+import { Link } from 'react-router-dom'
 
 export function FullPageErrorFallback({ error }) {
   const theme = useTheme()
-
+  console.log(theme)
   return (
     <div
       role="alert"
       css={{
-        color: theme.colors.red,
+        color: theme.colors.textColorDark,
         height: '100vh',
+        fontFamily: theme.font.body,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
       }}
     >
-      <p>Uh oh... There's a problem. Try refreshing the app.</p>
-      <pre>{error.message}</pre>
+      <p>
+        Uh oh... There's a problem. Try refreshing the app or navigating to <Link to="/">Home</Link>{' '}
+        and refreshing the app
+      </p>
+      <pre
+        css={{
+          color: theme.colors.red,
+        }}
+      >
+        {error.message}
+      </pre>
     </div>
   )
 }
